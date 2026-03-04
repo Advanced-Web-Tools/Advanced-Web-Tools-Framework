02bf32b20628225b5561a56d58f92cd0baa0d3e1
ElStefanos <stefancrkvenjakov1@gmail.com>
2026-03-04 14:59:59 +0100

    Cleanup of awt_package folder

--- awt_packages/MediaCenter/views/assets/js/MediaCenter/image/enlarge.js
+++
@@ -1,7 +0,0 @@
-import {PopulateHelper} from "../../../../../../Dashboard/js/ui/Helper.js";
-
-export function EnlargeImage(e) {
-    const index = e.currentTarget.getAttribute("data-index");
-    const image = document.querySelector("img[data-index='" + index + "']").cloneNode(true);
-    PopulateHelper(image.getAttribute("data-name"), image);
-}
