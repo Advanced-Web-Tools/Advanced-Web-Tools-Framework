f38ace8062a04177311dac3e821664b7d3369113
ElStefanos <stefancrkvenjakov1@gmail.com>
2026-03-14 17:14:05 +0100

    Replaced verbose error output in `awt_autoLoader.fun.php` with exception-based error handling for cleaner and more consistent class load failures.

--- awt_src/functions/awt_autoLoader.fun.php
+++ awt_src/functions/awt_autoLoader.fun.php
@@ -1,19 +1,13 @@
 <?php
-spl_autoload_register(function ($classname): void {
+spl_autoload_register(/**
+ * @throws Exception
+ */ function ($classname): void {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $classname);
     if (file_exists(CLASSES . $class . '.php')) {
         include_once CLASSES . $class . '.php';
     } else if (file_exists(PACKAGES . $class . ".php")) {
         include_once PACKAGES . $class . '.php';
     } else {
-        echo "Failed to load class: " . $class . "<br>";
-
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
-
-        foreach ($trace as $level) {
-            echo "File: " . ($level['file'] ?? '[internal]') . " Line: " . ($level['line'] ?? '?') . "<br>";
-            echo "Function: " . ($level['function'] ?? '[global]') . "<br><br>";
-        }
-        exit();
+        throw new Exception("Class $classname not found");
     }
 });
