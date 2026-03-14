4f183ead22c04d0520cb38f2c11fe70d66e01343
ElStefanos <stefancrkvenjakov1@gmail.com>
2026-03-14 17:24:26 +0100

    Refactored `RuntimeException` and `ERuntimeExceptionsMessage`:
    - Revised `ERuntimeExceptionsMessage` to remove trailing colons and align message formatting.
    - Updated `RuntimeException` to extend `ExceptionBase` for consistent error handling.
    - Introduced context-aware message building in `RuntimeException` for enhanced clarity.

--- awt_src/classes/packages/exceptions/RuntimeException.php
+++ awt_src/classes/packages/exceptions/RuntimeException.php
@@ -2,9 +2,22 @@
 
 namespace packages\exceptions;
 
-use Exception;
+use exception\ExceptionBase;
+use packages\exceptions\enums\ERuntimeExceptionsMessage;
 
-class RuntimeException extends Exception
+class RuntimeException extends ExceptionBase
 {
 
+    private ERuntimeExceptionsMessage $ERuntimeExceptionsMessage;
+
+    public function __construct(ERuntimeExceptionsMessage $ERuntimeExceptionsMessage, ?\Throwable $previous = null)
+    {
+        $this->ERuntimeExceptionsMessage = $ERuntimeExceptionsMessage;
+        parent::__construct($previous);
+    }
+
+    public function buildMessage(): string
+    {
+        return "{$this->context->contextName} runtime error. {$this->ERuntimeExceptionsMessage->value}";
+    }
 }
