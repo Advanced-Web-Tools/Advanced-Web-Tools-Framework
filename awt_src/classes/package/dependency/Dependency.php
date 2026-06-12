a3304d719edc031f177e9329c8c248ed982b0ebc
Stefan Crkvenjakov <stefancrkvenjakov1@gmail.com>
2026-06-12 16:09:14 +0200

    Refactor event dispatcher initialization and adjust `Dependency` class for improved immutability and consistency

--- awt_src/classes/package/dependency/Dependency.php
+++ awt_src/classes/package/dependency/Dependency.php
@@ -2,15 +2,15 @@
 
 namespace package\dependency;
 
-class Dependency
+readonly class Dependency
 {
     public function __construct(
-        public readonly string $name,
-        public readonly string $version,
-        public readonly string $url
+        public string $name,
+        public string $version,
+        public string $url
     ){}
 
-    public static function __fromArray(array $data): static
+    public static function fromArray(array $data): static
     {
         return new self($data['name'], $data['version'], $data['url']);
     }
